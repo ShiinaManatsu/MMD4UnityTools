994e63620a8a4e778aff48e165917d8760c687a9
Shiina Manatus <whitesky@live.cn>
2020-04-08 15:49:59 +0800

    License Update

--- MMD4UnityTools/Editor/MMDExtensionsEditor.cs
+++ MMD4UnityTools/Editor/MMDExtensionsEditor.cs
@@ -1,4 +1,24 @@
-﻿using MMDExtensions.Tools;
+﻿/*
+                                        DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+                                                Version 2, December 2004 
+
+                                Copyright (C) 2004 Sam Hocevar <sam@hocevar.net> 
+
+                                Everyone is permitted to copy and distribute verbatim or modified 
+                                copies of this license document, and changing it is allowed as long 
+                                as the name is changed. 
+
+                                        DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+                                TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+
+                                0. You just DO WHAT THE FUCK YOU WANT TO.
+ */
+
+ /*
+  Current Version: 1.1.0.1 +2
+  */
+
+using MMDExtensions.Tools;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
